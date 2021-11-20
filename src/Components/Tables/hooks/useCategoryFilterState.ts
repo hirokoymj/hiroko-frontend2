f17138cc6c03dff8759a9732ce4abf5d11f57c1f
Hiroko Yamaji <hiroko@hirokoymj.com>
2021-11-20 12:12:08 +0900

    convert typescript Title, Filter

--- src/Components/Tables/hooks/useCategoryFilterState.ts
+++ src/Components/Tables/hooks/useCategoryFilterState.ts
@@ -3,28 +3,28 @@ import { useQuery } from "@apollo/react-hooks";
 import get from "lodash/get";
 
 import { CATEGORY_ALL } from "Queries/Category";
+import { ICategory } from "Types/api/Category";
+
+type TCategoryOption = Pick<ICategory, "name" | "id">;
 
 export const useCategoryFilterState = () => {
-  const { data, loading: category_loading } = useQuery(CATEGORY_ALL);
+  const { data, loading: category_loading } = useQuery<ICategory>(CATEGORY_ALL);
   const [selectedFilters, setSelectedFilters] = React.useState([]);
   const categories = !category_loading && get(data, "categoryAll");
   const filters =
     !category_loading &&
-    categories.map(({ name, id }) => {
+    categories.map(({ name, id }: TCategoryOption) => {
       return {
         name: name,
         value: id,
       };
     });
 
-  const handleFilterChange = (event) => {
-    console.log("handleFilterChagne");
-    console.log(event.target);
-    console.log(event.target.value);
+  const handleFilterChange = (event: any) => {
     setSelectedFilters(event.target.value);
   };
 
-  const handleDeleteFilter = (value) => () => {
+  const handleDeleteFilter = (value: string) => () => {
     setSelectedFilters((chips) => {
       const result = chips.filter((chip) => chip !== value);
       return result;
--- src/Components/Titles/Title.tsx
+++ src/Components/Titles/Title.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   sectionTitle: {
     fontFamily: ["Titillium Web", "sans-serif"].join(","),
     marginBottom: theme.spacing(2),
   },
 }));
 
-export const Title = ({ text }) => {
+type Props = {
+  text: string;
+};
+
+export const Title = ({ text }: Props) => {
   const classes = useStyles();
   return (
     <Typography
