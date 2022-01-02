d5d3acd794dd851762e18d32bae7bed17370fb44
Hiroko Yamaji <hiroko@hirokoymj.com>
2022-01-02 13:44:00 +0900

    Used useLazyQuery to generate sub category options

--- src/Containers/TopicView.tsx
+++ src/Containers/TopicView.tsx
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
-import {
-  Field,
-  formValueSelector,
-  reduxForm,
-  InjectedFormProps,
-} from "redux-form";
+import { Field, reduxForm, InjectedFormProps } from "redux-form";
 import { useMutation } from "@apollo/react-hooks";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { useSnackbar } from "notistack";
-import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 
 import { TopicFormController } from "Components/FormController/TopicFormController";
@@ -22,11 +16,17 @@ import { TopicTable } from "Components/Tables/TopicTable";
 import { AlertDialog } from "Components/Dialog/AlertDialog";
 import { DELETE_TOPIC } from "Mutations/Topic";
 import { TOPICS } from "Queries/Topic";
-import { FormSkeleton } from "Components/Skeleton/FormSkeleton";
 import { TopicEditView } from "Containers/TopicEditView";
 import { makeStyles } from "@material-ui/core/styles";
 import { IDeleteTopicVars, ITopic } from "Types/api/Topic";
 import { TTopicFormData, IFormSelectOptions } from "Types/forms";
+import { useLazyQuery } from "@apollo/react-hooks";
+import { SUB_CATEGORY_BY_CATEGORY } from "Queries/SubCategory";
+import {
+  ISubCategory,
+  ISubCategoryByCategoryVars,
+} from "Types/api/SubCategory";
+import { makeDropdownOptions } from "Components/FormController/common";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -42,66 +42,73 @@ interface IProps extends InjectedFormProps<TTopicFormData> {
   category_options: IFormSelectOptions[];
   subCategory_options: IFormSelectOptions[];
   loading: boolean;
-  categoryId: string;
 }
 
 const TopicFormFields = (props: IProps) => {
   const classes = useStyles();
-  const {
-    handleSubmit,
-    submitting,
-    category_options,
-    subCategory_options,
-    loading,
-  } = props;
+  const { handleSubmit, submitting, category_options, loading } = props;
+
+  let options: any = [];
+  const [getSubCategoryByCategory, { data, loading: subCategoryLoading }] =
+    useLazyQuery<ISubCategory, ISubCategoryByCategoryVars>(
+      SUB_CATEGORY_BY_CATEGORY
+    );
+  if (!subCategoryLoading) {
+    options = makeDropdownOptions(
+      data,
+      "subCategoryByCategory",
+      subCategoryLoading
+    );
+  }
 
   return (
     <>
-      {loading ? (
-        <FormSkeleton fieldCount={4} />
-      ) : (
-        <>
-          <Field
-            name="category"
-            component={FormSelect}
-            fullWidth
-            variant="outlined"
-            label="Category"
-            options={category_options}
-          />
-          <Field
-            name="subCategory"
-            component={FormSelect}
-            fullWidth
-            variant="outlined"
-            label="Sub Category"
-            options={subCategory_options}
-          />
-          <Field
-            name="title"
-            component={FormTextField}
-            fullWidth
-            variant="outlined"
-            label="Title"
-          />
-          <Field
-            name="url"
-            component={FormTextField}
-            fullWidth
-            variant="outlined"
-            label="URL"
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            disabled={submitting}
-            onClick={handleSubmit}
-            className={classes.button}>
-            {submitting ? "Submitting" : "Submit"}
-          </Button>
-        </>
-      )}
+      <Field
+        name="category"
+        component={FormSelect}
+        fullWidth
+        variant="outlined"
+        label="Category"
+        options={category_options}
+        onChange={(event: any, newVal: string) => {
+          getSubCategoryByCategory({
+            variables: { categoryId: newVal },
+          });
+        }}
+        disabled={loading || subCategoryLoading}
+      />
+      <Field
+        name="subCategory"
+        component={FormSelect}
+        fullWidth
+        variant="outlined"
+        label="Sub Category"
+        options={options}
+        disabled={loading || subCategoryLoading}
+      />
+      <Field
+        name="title"
+        component={FormTextField}
+        fullWidth
+        variant="outlined"
+        label="Title"
+      />
+      <Field
+        name="url"
+        component={FormTextField}
+        fullWidth
+        variant="outlined"
+        label="URL"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        disabled={submitting}
+        onClick={handleSubmit}
+        className={classes.button}>
+        {submitting ? "Submitting" : "Submit"}
+      </Button>
     </>
   );
 };
@@ -110,14 +117,7 @@ const TopicForm = reduxForm<TTopicFormData, IProps>({
   form: "Create_Topic_Form",
 })(TopicFormFields);
 
-interface ITopicView {
-  categoryId: string;
-}
-
-export const TopicView = connect((state) => ({
-  categoryId: formValueSelector("Create_Topic_Form")(state, "category"),
-}))((props: ITopicView) => {
-  const { categoryId } = props;
+export const TopicView = () => {
   const { enqueueSnackbar } = useSnackbar();
   const [open, setOpen] = useState<boolean>(false);
   const [topicId, setTopicId] = useState<string>("");
@@ -166,7 +166,7 @@ export const TopicView = connect((state) => ({
         <Grid container spacing={3} justify="center">
           <Grid item xs={12} md={6}>
             <Paper>
-              <TopicFormController categoryId={categoryId}>
+              <TopicFormController>
                 {(props: any) => <TopicForm {...props} />}
               </TopicFormController>
             </Paper>
@@ -196,4 +196,4 @@ export const TopicView = connect((state) => ({
       </DashboardLayout>
     </>
   );
-});
+};
